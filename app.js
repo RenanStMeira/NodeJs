d25c92f6588fc28e60d5fca55d3b7efd8ca61eb0
Renan <renan.meira25@gmail.com>
2023-05-03 16:04:00 -0300

    adicionando app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // incluindo uma biblioteca do HTTP
 import * as http from 'http';
 import * as url from 'url';
- // Incluido biblioteca de URL
 import queryString from 'query-string'; // incluindo pacote
-
-
+import * as fs from 'fs';
 
 // Definição de endereço / URL
 const hostname = '127.0.0.1'; // localhost
@@ -14,35 +12,63 @@ const port = 3030;
 // Implementação da regra de megocio
 const server = http.createServer((req, res) => {
 
-    // Pegar a pergunta na URL
-    const params = queryString.parse(url.parse(req.url, true).search);
-
-    // Verificar a pergunta e escolher a resposta
-    let resposta;
-
-    if(params.pergunta == 'melhor filme de ação' ) {
-      resposta = 'Top Gun';
-    } 
-
-    else if(params.pergunta == 'melhor tecnologia backend' ) {
-      resposta = 'nodejs';
-    }  
-    
-    else {
-      resposta = 'Nao sei';
-    }
+  var resposta;
+  const urlParse = url.parse(req.url, true);
+  // Receber os dados do usuario
+  const params = queryString.parse(urlParse.search);
 
 
-    // retornasr a resposta
+  // Criar um usuario -- Atualizar
+  if(urlParse.pathname == '/criar-usuario'){
 
+        // Salvar as informações (pequisar na biblioteca npmjs *file* ou fs)
+                  //Pasta de destino  //Id do usuario                      //Function de calback
+        fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+          if (err) throw err; // Retorno da resposta 
+          console.log('Saved!');
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(resposta);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end(resposta);
+        });
+        
+        resposta = 'Usuario criado com sucesso';
+
+  }
+
+  
+  // Selecionar um usuario    
+    else if(urlParse.pathname == '//selecionar-usuario') {
+      fs.readFile('users/' + params.id + '.txt', function(err, data) {
+        resposta = data;
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(resposta);
+       });
+    }   
+
+  // Remover o usuario
+  else if(urlParse.pathname == '//remover-usuario') {
+    fs.unlink('users/' + params.id + '.txt', function (err) {
+      console.log('deletado')
+
+      res.statusCode = 204;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+
+    });
+
+  }
 });
 
 
     // Execução
-server.listen(port, hostname, () => {
+  server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+//http://localhost:3030/criar-usuario?nome=Renan&idade=31&id=1
+//http://localhost:3030/selecionar-usuario?id=2
+//http://localhost:3030/remover-usuario?id=2
